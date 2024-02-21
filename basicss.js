a6daa6f8ec6b0be4bc67fe187e3c2cb33c00ff3c
Yaseeeen <halfduplex001@gmail.com>
2024-02-21 17:02:49 +0530

    first commit

---
+++ basicss.js
@@ -0,0 +1,90 @@
+//let x=10;
+//console.log(x);
+//{
+    //let x=50;
+   // console.log(x);
+//}
+//a=true;
+//console.log(a);
+//console.log(typeof(a));
+//temp=26;
+//if(temp>30)
+//{
+    //console.log("its a hot day");
+//}
+//else if(temp<=30 && temp>20)
+//{
+    //console.log("its a pleasant day");
+//}
+//else{
+    //console.log("its a cold day");
+//}
+//for(let i=0;i<5;i++)
+//{
+    //console.log(i);
+//}
+ //let fruits=["apple","banana","orange"]
+// console.log(fruits[1]);
+// console.log(fruits.length);
+// fruits[2]='grapes';
+// console.log(fruits);
+// for(let)
+// fruits.push('kiwi');
+// console.log(fruits)
+// fruits.pop();
+// console.log(fruits)
+// fruits.shift();
+// console.log(fruits);
+// function greet(name)
+// {
+//     console.log('welcome'+''+name+'!');
+// }
+// greet(khadeeja)
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(2,3));
+// let people=[
+//     {
+//     name:"hayri",
+//     age:1,
+//     place:{
+//     city:"clt",
+//     state:"kerala",
+//     }
+// },
+// {
+//     name:"salih",
+//     age:50,
+//     place:{
+//     city:"tvm",
+//     state:"kerala",
+//     }
+// },
+// {
+//     name:"marji",
+//     age:40,
+//     place:{
+//     city:"kannur",
+//     state:"kerala",
+//     }
+// }
+// ]
+// console.log(people[0].name);
+// var add=(a,b){
+//     var sum=a+b
+//     console.log(sum)
+//     console.log("sum is $(sum)")
+// }
+// add(10,30)
+var [a,b,c,d]=["apple","orange","banana","grapes"]
+console.log(a);
+console.log(b);
+var a=[1,2,3];
+var b=[4,5,6];
+var c=[...a,...b];
+console.log(c);
+var[a,b,...t]=[2,3,4,5,6];
+console.log(t);
+console.log("first element of t is"+t(0));
